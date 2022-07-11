0fd5b67549365acf3a666f5087169f2785ae494e
LadeMor <sergpyatko@gmail.com>
2022-07-11 15:36:06 +0300

    Added search engine to main page

--- src/componens/main/Main.js
+++ src/componens/main/Main.js
@@ -9,30 +9,85 @@ import PostCard from "../post/postcard/PostCard";
 
 function Main(){
 
+    const [data, setData] = useState(null);
+    const [filteredData, setFilteredData] = useState(null);
+    const [searchData, setSearchData] = useState(null);
     const [posts, setPosts] = useState(null);
     const url = 'https://localhost:5001/api/Post';
 
     useEffect(() => {
         fetch(url)
             .then((response) => response.json())
-            .then((actualData) => setPosts(actualData));
+            .then((actualData) => {
+                setPosts(actualData);
+                setData(actualData);
+            })
     }, []);
+
+    const handleChange = (e) => {
+        setSearchData(e.target.value.toLowerCase());
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchData.trim() === ''){
+            setData(posts);
+        }else if(posts.filter(post => post.title.toLowerCase().includes(searchData)) == ''){
+            console.log('No result');
+        } else{
+            setData(posts
+                .filter(post => post
+                    .title
+                    .toLowerCase()
+                    .includes(searchData)));
+        }
+
+
+
+    };
 
     return(
         <div>
-            <div className="content-block">
-                <div className="content-block-tools">
-                    <ul>
-                        <li>
-                            <img src={filter}/>
-                        </li>
-                        <li>
-                            <img src={sort}/>
-                        </li>
-                    </ul>
-                </div>
-                <PostCard posts={posts}/>
+            <div className="content-block-tools">
+                <ul>
+                    <li>
+                        <img src={filter}/>
+                    </li>
+                    <li>
+                        <img src={sort}/>
+                    </li>
+                </ul>
             </div>
+            <div className="content-block">
+                <div className="search-from">
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Search" onChange={handleChange}/>
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+                <PostCard posts={data}/>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/><br/>
+            <br/>
+            <br/><br/>
+            <br/>
+            <br/>
+
+
         </div>
     );
 }
