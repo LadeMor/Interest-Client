3b3d8901aff6c5ed00d347ca501906db3cacd4d9
LadeMor <sergpyatko@gmail.com>
2022-07-26 15:22:32 +0300

    Resolved problem with register validation

--- src/componens/auth/registration/Register.js
+++ src/componens/auth/registration/Register.js
@@ -1,38 +1,138 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./Register.css";
 
 
 function Register(){
+
+    const [username, setUsername] = useState('');
+    const [description, setDescription] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [profilePhoto, setProfilePhoto] = useState('');
+
+
+    async function isUserExistByUsername (userDb) {
+        const urlUsername = `https://localhost:5001/api/User/${userDb.username}/exist`;
+
+         const resUsername = await fetch(urlUsername)
+            .then(responseUsername => responseUsername.json())
+            .then(dataUsername => dataUsername);
+
+        return resUsername;
+    }
+
+    async function isUserExistByEmail (userDb) {
+        const urlEmail = `https://localhost:5001/api/User/${userDb.email}/user`;
+
+        const resEmail = await fetch(urlEmail)
+            .then(responseEmail => responseEmail.json())
+            .then(dataEmail => dataEmail);
+
+        return resEmail;
+    }
+
+    function addUser(usernameB, emailB, passwordB){
+        if(usernameB && emailB && passwordB){
+            console.log("Good!");
+        }
+    }
+
+    async function validateUserData(userDb, sendReq){
+
+        let usernameIsCorrect = false;
+        let emailIsCorrect = false;
+        let passwordIsCorrect = false;
+
+        if(userDb.username.trim() !== ''){
+            await isUserExistByUsername(userDb).then(res => {
+                if(res){
+                    usernameIsCorrect = false;
+                    console.log('This username is exist!');
+                }else{
+                    usernameIsCorrect = true;
+                }
+            });
+        }else{
+            usernameIsCorrect = false;
+            console.log('Enter username!');
+        }
+
+        if(userDb.email.trim() !== ''){
+            await isUserExistByEmail(userDb).then(res => {
+                if(res){
+                    emailIsCorrect = false;
+                    console.log('This email is exist!');
+                }else{
+                    emailIsCorrect = true;
+                }
+            });
+        }else{
+            emailIsCorrect = false;
+            console.log('Enter email!');
+        }
+
+        if(userDb.password.trim() !== ''){
+            passwordIsCorrect = true;
+        }else{
+            passwordIsCorrect = false;
+            console.log('Enter password!');
+        }
+
+        sendReq(usernameIsCorrect, emailIsCorrect, passwordIsCorrect);
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const userData = {
+            username: username,
+            password: password,
+            email: email,
+            description: description,
+            roleId:2
+        };
+
+        validateUserData(userData, addUser);
+
+    }
+
     return(
         <div className="main-block">
             <div className="form-block-register">
                 <h1>Registration</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <label>Username</label>
                     <input
+                        className = 'error-value'
                         type="text"
                         name="username"
+                        onChange={e => setUsername(e.target.value)}
                     />
                     <label>Description</label>
                     <textarea name="description"
-                              maxlength="200">
+                              maxLength="200"
+                              onChange={e => setDescription(e.target.value)}>
 
                     </textarea>
                     <label>Email</label>
                     <input
                         type="email"
                         name="email"
+                        onChange={e => setEmail(e.target.value)}
                     />
                     <label>Password</label>
                     <input
                         type="password"
                         name="password"
+                        onChange={e => setPassword(e.target.value)}
                     />
                     <label>Profile photo</label>
                     <input
                         type="text"
                         name="profilpic"
+                        onChange={e => setProfilePhoto(e.target.value)}
                     />
                     <button type="submit">
                         Submit
