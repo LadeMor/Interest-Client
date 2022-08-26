0b394abab7cbcdbb2c081f0f11f6079fc322cb2e
LadeMor <sergpyatko@gmail.com>
2022-08-26 16:48:32 +0300

    Finished delete post button logic

--- src/componens/post/post preview/PostPreview.js
+++ src/componens/post/post preview/PostPreview.js
@@ -1,22 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import './PostPreview.css';
 
 function PostPreview({postData}){
+
+    const url = `https://localhost:5001/api/Post/${postData.id}`;
+
+    const deleteItem = (e) => {
+        e.preventDefault();
+
+        let confimDelete = window.confirm("Are you sure you want to delete this post?");
+        if(confimDelete){
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(() => {
+                window.location.replace('/');
+            })
+        }
+
+    }
 
     return(
         <div className='post-preview'>
             <h1>{postData.title}</h1>
             <img src={`${postData.image}`}/>
             <p>{postData.post_Description}</p>
-            {+localStorage.getItem('UserId') === postData.user_Id ?
+            {+localStorage.getItem('UserId') === postData.user_Id || +localStorage.getItem('UserRoleId') === 1?
                 <div className='post-page-buttons'>
                     <button className='post-page-edit-button'>Edit</button>
-                    <button className='post-page-delete-button'>Delete</button>
+                    <button className='post-page-delete-button' onClick={deleteItem}>Delete</button>
                 </div> :
                 ''
             }
-
         </div>
     );
 }
