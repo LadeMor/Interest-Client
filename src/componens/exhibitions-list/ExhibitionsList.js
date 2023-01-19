4672c047e578299da5f8ff1806f62c15de337c15
LadeMor <sergpyatko@gmail.com>
2023-01-19 14:09:32 +0200

    Added layout to exhibition system

---
+++ src/componens/exhibitions-list/ExhibitionsList.js
@@ -0,0 +1,60 @@
+import React, {useState} from "react";
+import {
+    Card,
+    CardMedia,
+    Typography,
+    CardActions,
+    Button,
+    CardContent} from "@mui/material";
+
+import landscape from "../../img/exhibition-photo-titles/mountain-landscape-g81e3a8da4_1920.jpg";
+import imageRav from "../../img/exhibition-photo-titles/1a34261c15c100fe3ce4dd14e99ea507.jpg";
+
+function ExhibitionsList(){
+
+    const [exhibitionList, setExhibitionList] = useState([
+        {
+            id: 1,
+            image: landscape,
+            title: "Landscapes",
+            description: "Lizards are a widespread group of squamate reptiles, with over 6,000\n" +
+                "                        species, ranging across all continents except Antarctica"
+        },
+        {
+            id: 2,
+            image: imageRav,
+            title: "Arts",
+            description: "There are exhibition about arts"
+        }
+    ]);
+
+    return (
+        <>
+            {exhibitionList && exhibitionList.map(item => (
+                <Card sx={{ marginX: 10, marginY: 5 }} key={item.id}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={`${item.image}`}
+                        title="green iguana"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {item.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {item.description}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Share</Button>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                )
+            )}
+
+        </>
+    )
+}
+
+export default ExhibitionsList;
