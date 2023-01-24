24b3fc8000806e5037449bd742342315e1caf8cf
LadeMor <sergpyatko@gmail.com>
2023-01-24 14:25:01 +0200

    Added back arrow button on post page

--- src/componens/post/post page/PostPage.js
+++ src/componens/post/post page/PostPage.js
@@ -2,12 +2,9 @@ import React, {useEffect, useState} from "react";
 import {Link, useParams, useNavigate} from "react-router-dom";
 import PostPreview from "../post preview/PostPreview";
 import {getPost} from "../../../services/post-service/PostService";
-import Container from '@mui/material/Container';
-
-
-
-import arrow_back from '../../../icons/arrow-back.svg';
-
+import {Button, Container, IconButton} from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { redirect } from "react-router-dom";
 
 function PostPage(){
 
@@ -26,11 +23,16 @@ function PostPage(){
 
     return(
         <>
-            {/*<div className='back_arrow_block'>*/}
-            {/*    <Link to="/">*/}
-            {/*        <img src={arrow_back}/>*/}
-            {/*    </Link>*/}
-            {/*</div>*/}
+            <IconButton
+                variant="contained"
+                sx={{position:"absolute", left:200}}
+                size="large"
+                onClick={() => navigate(-1)}>
+                <ArrowBackIcon
+                    color="primary"
+                    aria-label="arrow back"
+                    />
+            </IconButton>
             <Container>
                 {(postArr === undefined || postArr === null || postArr === [] ?
                         "Loading" :
