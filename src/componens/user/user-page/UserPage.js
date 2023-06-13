423893115ec5ffd339d44db6e9a882fb1a75db76
LadeMor <sergpyatko@gmail.com>
2023-06-13 15:06:12 +0300

    Added styles on user page

--- src/componens/user/user-page/UserPage.js
+++ src/componens/user/user-page/UserPage.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
-
-
+import './UserPage.css';
 import {Link} from "react-router-dom";
 import {getAllPosts} from "../../../services/post-service/PostService";
 import PostCards from "../../post/post-cards/PostCards";
@@ -39,18 +38,21 @@ function UserPage(){
                 <div>
                     <Container>
                         <Avatar
-                            alt="Remy Sharp"
+                            alt={`${user.username}'s profile photo`}
                             src={userPhoto}
                             sx={{ width: 200, height: 200 }}
                         />
                         <Typography variant="h3" component="h2">
-                            {user.username}
+                            {user.username} - <span className="author-mark">Author</span>
                         </Typography>
                         <Typography variant="body1" gutterBottom>
                             {user.userDescription}
                         </Typography>
                         <Link to="/post_create" >
-                            <Button variant="contained" style={{marginBottom:"10px"}}>Create post</Button>
+                            <Button variant="contained" style={{marginBottom:"10px", padding:'15px'}}>Create post</Button>
+                        </Link>
+                        <Link to="/exhibition/exhibition_create" >
+                            <Button variant="contained" style={{marginBottom:"10px", marginLeft:'10px', padding:'15px'}}>Create exhibition</Button>
                         </Link>
                         <div className="user-page-cards">
                             <PostCards post={user.posts}/>
